21f847e9b191155f3624cb04a64bef190ccc0086
Ivana Belinić <belinicivana15@gmail.com>
2023-04-07 21:32:25 +0100

    Okruženje_Dodaci

---
+++ Software/AcceptanceTests/AcceptanceTests/Support/GuiDriver.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Appium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcceptanceTests.Support
+{
+    public static class GuiDriver
+    {
+        private static WindowsDriver<WindowsElement> driver;
+        public static WindowsDriver<WindowsElement> GetOrCreateDriver()
+        {
+            if (driver == null)
+            {
+                driver = CreateDriverInstance();
+            }
+            return driver;
+        }
+        public static WindowsDriver<WindowsElement> GetDriver()
+        {
+            return driver;
+        }
+        private static WindowsDriver<WindowsElement> CreateDriverInstance()
+        {
+            var options = new AppiumOptions();
+            options.AddAdditionalCapability("app", @"C:\Program Files (x86)\Default Company Name\Setup1\");
+            options.AddAdditionalCapability("deviceName", "WindowsPC");
+            var wd = new WindowsDriver<WindowsElement>(new Uri("http://192.168.56.1"),
+           options);
+            wd.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            return wd;
+        }
+        public static void Dispose()
+        {
+            foreach (var wh in driver.WindowHandles)
+            {
+                driver.SwitchTo().Window(wh);
+                driver.CloseApp();
+            }
+            driver.Quit();
+            driver.Dispose();
+            driver = null;
+        }
+    }
+}
