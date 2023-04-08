a7b2980d5cf5daa62790141383f25deafd922a0c
lgrzelj <lgrzelj@student.foi.hr>
2023-04-08 14:46:12 +0200

    Kreiran Feature UserRefistration-Scenarij 1, također i datoteka StepDefiniton.

---
+++ Software/AcceptanceTests/AcceptanceTests/Features/UserRegistration.feature
@@ -0,0 +1,52 @@
+﻿Feature: UserRegistration
+
+Application needs to provide registration for user
+because it will have priviligies based on his role.
+
+Background:
+   Given User is on welcome form
+   When User clicks button "Registracija"
+
+Given User is on the registration form
+    When User enters the following details:
+    | ime      | Anastazija                  |
+    | prezime  | Stazić                      |
+    | adresa   | Kolodvorska 60              |
+    | email    | anastazija.stazic@gmail.com |
+    | broj     | 0911234567                  |
+    | korisnik | anastazija                  |
+    | lozinka  | anastazija                  |
+    | uloga    | Vlasnik                     |
+    And User clicks the "Registriraj me"
+    Then User should be successfully redirected to the welcome form of the application.
+
+Scenario: Invalid e-mail address
+   When User fills the form with data that follows:
+    | ime      | Karlo         |
+    | prezime  | Stazić        |
+    | adresa   | Karlova ulica |
+    | email    | karlo         |
+    | broj     | 0911237654    |
+    | korisnik | karlo         |
+    | lozinka  | karlo         |
+   And User click "Registriraj me"
+   Then System will display "Neispravno unesena e-mail adresa" on form
+
+
+  Scenario: Invalid Phone Number
+    When user fills in the details as follows:
+      | Field         | Value             |
+      | Name          | Karlo             |
+      | Surname       | Stazić            |
+      | Address       | Karlova ulica     |
+      | Email         | karlo@gmail.com   |
+      | Phone number  | broj              |
+      | Username      | karlo             |
+      | Password      | karlo             |
+      | Role          | Vlasnik           |
+    And clicks the "Registriraj me" button
+    Then system should display an error message in the form of a popup on the screen saying "Broj telefona mora sadržavati 10 znamenaka."
+
+Scenario: Registration cancellation
+When user clicks on the "Cancel" button
+Then system should display the wlcome form of the application
---
+++ Software/AcceptanceTests/AcceptanceTests/StepDefinitions/UserRegistrationStepDefinitions.cs
@@ -0,0 +1,93 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace AcceptanceTests.StepDefinitions
+{
+    [Binding]
+    public class UserRegistrationStepDefinitions
+    {
+        [Given(@"User is on welcome form")]
+        public void GivenUserIsOnWelcomeForm()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"User clicks button ""([^""]*)""")]
+        public void WhenUserClicksButton(string registracija)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"User is on the registration form")]
+        public void GivenUserIsOnTheRegistrationForm()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"User enters the following details:")]
+        public void WhenUserEntersTheFollowingDetails(Table table)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"User clicks the ""([^""]*)""")]
+        public void WhenUserClicksThe(string p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"User should be successfully redirected to the welcome form of the application\.")]
+        public void ThenUserShouldBeSuccessfullyRedirectedToTheWelcomeFormOfTheApplication_()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"User fills the form with data that follows:")]
+        public void WhenUserFillsTheFormWithDataThatFollows(Table table)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"User click ""([^""]*)""")]
+        public void WhenUserClick(string p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"System will display ""([^""]*)"" on form")]
+        public void ThenSystemWillDisplayOnForm(string p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"user fills in the details as follows:")]
+        public void WhenUserFillsInTheDetailsAsFollows(Table table)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"clicks the ""([^""]*)"" button")]
+        public void WhenClicksTheButton(string p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"system should display an error message in the form of a popup on the screen saying ""([^""]*)""")]
+        public void ThenSystemShouldDisplayAnErrorMessageInTheFormOfAPopupOnTheScreenSaying(string p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"user clicks on the ""([^""]*)"" button")]
+        public void WhenUserClicksOnTheButton(string cancel)
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"system should display the wlcome form of the application")]
+        public void ThenSystemShouldDisplayTheWlcomeFormOfTheApplication()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
